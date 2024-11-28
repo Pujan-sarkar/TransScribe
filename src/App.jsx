4b116b5d17fd7597fc2fa3b91044967336ef573a
Pujan-sarkar <pujansarkar37@gmail.com>
2024-11-28 21:47:43 +0530

    feat: add some more imp files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
 import FileDisplay from "./components/FileDisplay";
 import Information from "./components/Information";
 import Transcribing from "./components/Transcribing";
-
-
+import { MessageTypes } from "./utils/presets";
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
   const [output, setOutput] = useState(null);
+  const [downloading, setDownloading] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [finished, setFinished] = useState(false);
 
   const isAudioAvailable = file || audioStream;
   function handleAudioReset() {
@@ -19,16 +20,75 @@ function App() {
     setAudioStream(null);
   }
 
+  const worker = useRef(null);
+
   useEffect(() => {
-    console.log(audioStream);
-  }, [audioStream]);
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("./utils/whisper.worker.js", import.meta.url),
+        {
+          type: "module",
+        }
+      );
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case "DOWNLOADING":
+          setDownloading(true);
+          console.log("DOWNLOADING");
+          break;
+        case "LOADING":
+          setLoading(true);
+          console.log("LOADING");
+          break;
+        case "RESULT":
+          setOutput(e.data.results);
+
+          break;
+        case "INFERENCE_DONE":
+          setFinished(true);
+          console.log("DONE");
+          break;
+      }
+    };
+
+    worker.current.addEventListener("message", onMessageReceived);
+
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
+  }, []);
+
+  async function readAudioForm(file) {
+    const sampling_rate = 16000;
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+    const respond = await file.arrayBuffer();
+    const decode = await audioCTX.decodeAudioData(response);
+    const audio = decode.getChannelData(0);
+    return audio;
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) {
+      return;
+    }
+
+    let audio = await readAudioForm(file ? file : audioStream);
+    const model_name = `openai/whisper-tiny.en`;
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name,
+    });
+  }
 
   return (
     <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
         {output ? (
-          <Information/>
+          <Information />
         ) : loading ? (
           <Transcribing />
         ) : isAudioAvailable ? (
